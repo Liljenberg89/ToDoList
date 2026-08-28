f61c4e2336c6d3f2f34d9df98cd3fc126f3f6cfa
Filip Liljenberg <filipliljenberg9@gmail.com>
2026-08-28 20:55:59 +0200

    Add task has functionality

--- ToDo-Frontend/src/App.tsx
+++ ToDo-Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, type Dispatch } from "react";
 import heroImg from "./assets/hero.png";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
@@ -6,18 +6,68 @@ import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState(["Städa", "Handla", "Laga mat"]);
+  const [showAddTask, setShowAddTak] = useState<boolean>(false);
+
+  const AddTask = () => {
+    const [newTask, setNewTask] = useState({
+      task: "",
+      description: "",
+      time: "",
+    });
+
+    const addTask = (e: any) => {
+      e.preventDefault();
+
+      // Lägg till task här
+      console.log(newTask);
+    };
+    return (
+      <div>
+        <form onSubmit={addTask}>
+          <input
+            type="text"
+            placeholder="Task"
+            value={newTask.task}
+            onChange={(e) =>
+              setNewTask((prev) => ({ ...prev, task: e.target.value }))
+            }
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            value={newTask.description}
+            onChange={(e) =>
+              setNewTask((prev) => ({ ...prev, description: e.target.value }))
+            }
+          />
+          <input
+            type="text"
+            placeholder="Time"
+            value={newTask.time}
+            onChange={(e) =>
+              setNewTask((prev) => ({ ...prev, time: e.target.value }))
+            }
+          />
+          <input type="submit" value="Add task"></input>
+        </form>
+      </div>
+    );
+  };
 
   return (
-    <body>
+    <div className="container">
       <div className="tasks">
         <ul>
           {tasks.map((task) => (
             <div className="task">{task}</div>
           ))}
         </ul>
-        <button className="addTask">Add task </button>
+        <button className="addTask" onClick={() => setShowAddTak(!showAddTask)}>
+          Add task{" "}
+        </button>
+        {!showAddTask ? "" : <AddTask />}
       </div>
-    </body>
+    </div>
   );
 }
 
